a28b1f92d0ec4bd0fd71dd76cd0c25bf625ad8f9
Landlord534 <nikolay.chalykh.job@gmail.com>
2022-12-26 09:13:16 +0300

    Project initialization

---
+++ src/main/java/com/chalykh/blog/ChalykhBlogApplication.java
@@ -0,0 +1,13 @@
+package com.chalykh.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalykhBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChalykhBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chalykh/blog/ChalykhBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chalykh.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChalykhBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
